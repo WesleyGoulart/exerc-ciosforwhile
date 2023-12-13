f6c0d818a0bba6b4da6b138e632b9b63e1f74ebd
Wesley Goulart <goulart89@gmail.com>
2023-12-13 15:57:41 -0300

    ex3 - feito com for, mas não imprimindo o 2000

--- ex3.js
+++ ex3.js
@@ -1,5 +1,6 @@
 // Faça um programa que verifique e mostre os números entre 1.000 e 2.000 (inclusive) que, quando divididos por 11 produzam resto igual a 2.
 
+/*
 let x = 1000
 const y = 2000
 let resto2
@@ -10,4 +11,14 @@ while (x <= y) {
         console.log(x)
     }
     x++
+}
+*/
+
+
+for (let x = 1000; x <= 2000; x++) {
+    let resto2
+    resto2 = x % 11
+    if (resto2 == 2) {
+        console.log(x)
+    }
 }
