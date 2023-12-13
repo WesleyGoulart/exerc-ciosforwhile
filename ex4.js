5678c49ced08c884b1ba7d9fd2e312d2a3e9d257
Wesley Goulart <goulart89@gmail.com>
2023-12-13 16:51:07 -0300

    ex4 - feito com while

---
+++ ex4.js
@@ -0,0 +1,12 @@
+// Faça um programa que leia um valor n, inteiro e positivo, calcule e mostre a seguinte soma: S = 1 + 1/2 + 1/3 + 1/4 + … + 1/n.
+
+const n = 5
+let x = 1
+let soma = 1
+
+
+while (x < n) {
+    x++
+    soma = soma + 1/x
+}
+console.log(soma)
