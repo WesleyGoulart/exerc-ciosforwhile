643dffc79e63a3e768be49f01191683cdf5b8ae2
Wesley Goulart <goulart89@gmail.com>
2023-12-13 15:59:55 -0300

    ex3 - completo

--- ex3.js
+++ ex3.js
@@ -1,20 +1,20 @@
 // Faça um programa que verifique e mostre os números entre 1.000 e 2.000 (inclusive) que, quando divididos por 11 produzam resto igual a 2.
 
-/*
+
 let x = 1000
-const y = 2000
 let resto2
 
-while (x <= y) {
+while (x <= 2000) {
     resto2 = x % 11
     if (resto2 == 2) {
         console.log(x)
     }
     x++
 }
-*/
 
 
+
+/*
 for (let x = 1000; x <= 2000; x++) {
     let resto2
     resto2 = x % 11
@@ -22,3 +22,5 @@ for (let x = 1000; x <= 2000; x++) {
         console.log(x)
     }
 }
+
+*/
