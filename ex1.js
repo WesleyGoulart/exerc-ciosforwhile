c5fe2789cfce02152edb16b265a91d0f1b477147
Wesley Goulart <goulart89@gmail.com>
2023-12-13 13:25:38 -0300

    Exercicio feito com for

--- ex1.js
+++ ex1.js
@@ -1,5 +1,6 @@
 // Faça um programa que receba um número e usando laços de repetição calcule e mostre a tabuada desse número.
 
+/* 
 const x = 2
 let y = 0
 let produto
@@ -8,4 +9,13 @@ while (y <= 10) {
     produto = x * y
     console.log(`${x} x ${y} = ${produto}`)
     y++
+}
+*/
+
+const x = 2
+
+for (let y = 0; y <= 10; y++) {
+    let produto
+    produto = x * y
+    console.log(`${x} x ${y} = ${produto}`)
 }
